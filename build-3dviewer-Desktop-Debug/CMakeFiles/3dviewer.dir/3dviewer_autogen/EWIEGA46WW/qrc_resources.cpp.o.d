5a0a060d4bf330989cd14437456ea6bb5fb9c815
Grance Tijuana <granceti@se-i6.nsk.21-school.ru>
2024-11-09 07:46:33 +0700

    Бесповоротный коммит

--- build-3dviewer-Desktop-Debug/CMakeFiles/3dviewer.dir/3dviewer_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/3dviewer.dir/3dviewer_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
- /Users/granceti/Projects/cpp/3DViewer_CPP/build-3dviewer-Desktop-Debug/3dviewer_autogen/EWIEGA46WW/qrc_resources.cpp
